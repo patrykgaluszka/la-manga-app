4fdb53a0e7c81b4f323e7d5fd3ad96389048cedd
Patryk <patryk@Mac-mini-User.local>
2025-10-16 22:58:05 +0200

    fix build errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,10 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import staticAdapter from '@astrojs/adapter-static';
 
 export default defineConfig({
   integrations: [react(), mdx(), tailwind()],
   output: 'static',
-  adapter: staticAdapter(),
   site: 'https://patrykgaluszka.github.io/la-manga-app',
   base: '/la-manga-app',
 });
--- src/components/VideoModal.tsx
+++ src/components/VideoModal.tsx
@@ -43,7 +43,6 @@ export default function VideoModal({ videoId }: VideoModalProps) {
                 className="absolute top-0 left-0 w-full h-full"
                 src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
                 title="YouTube video player"
-                frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
               ></iframe>
